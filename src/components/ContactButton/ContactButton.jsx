abfee11eb5098c2e0e41ce6c22ac45c73ee7ba47
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2023-01-31 14:19:32 -0300

    fix: ContactButton fixed

--- src/components/ContactButton/ContactButton.jsx
+++ src/components/ContactButton/ContactButton.jsx
@@ -5,19 +5,21 @@ import { Form } from "../Form/Form"
 
 export const ContactButton = ({textoBoton}) =>{
     let textoBtn = textoBoton? textoBoton : "Contacto"
-    const [modal, setModal] = useState(true);
+    const [modal, setModal] = useState(false);
 
     function modalClick(){
-        modal? setModal(false) : setModal(true)
+        modal? setModal(false) : setModal(true)       
     }
     return (
+        <>
         <button className="contact-btn" onClick={modalClick}>  
-            {textoBtn}
-            <Modal show={modal} onClose={modalClick} position={"center"} dismissible={true}>
-                <div id="modalFormConteiner">
-                    <Form/>
-                </div>
-            </Modal>
+            <p>{textoBtn}</p>
         </button>
+        <Modal show={modal} onClose={modalClick} position={"center"} dismissible={true}>
+            <div id="modalFormConteiner">
+                <Form/>
+            </div>
+        </Modal>
+        </>
     )
 }
