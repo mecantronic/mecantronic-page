90839867a451391558c4db80edfe685eb73e78f5
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2023-02-18 10:57:54 -0300

    fix: cambio de numero de contacto WhatsApp Component

--- src/components/WhatsAppBtn/WhatsAppBtn.jsx
+++ src/components/WhatsAppBtn/WhatsAppBtn.jsx
@@ -2,7 +2,7 @@ import "./WhatsAppBtn.css"
 
 export const WhatsAppBtn = () =>{
     return (
-        <a className="whatsAppBtn" href="https://wa.me/5492644647572" target="_blank" rel="noreferrer">
+        <a className="whatsAppBtn" href="https://wa.me/5491157397834" target="_blank" rel="noreferrer">
             <img src="../assets/icons/whatsApp.png" alt="WhatsApp MECANTRONIC"/>
         </a>
     )
