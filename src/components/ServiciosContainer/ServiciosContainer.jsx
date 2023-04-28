831712ba21faf984c019f5ebeffca42190384a3c
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2023-04-28 13:56:44 -0300

    fix: modificacion responsividad de sevicios container

--- src/components/ServiciosContainer/ServiciosContainer.jsx
+++ src/components/ServiciosContainer/ServiciosContainer.jsx
@@ -21,19 +21,19 @@ export const ServiciosContainer = ({ serviceToShow }) => {
             title={serviceToShow["sobrePrincipal"]}
           />
           <div className="flex sobrecontainer">
-          <div className="w-1/2 pl-20">
-            <img  className="sobreimg" src={`../assets/${serviceToShow["sobreImg"]}`} alt="" />
-          </div>
-          <div className="w-1/2 sobreinfocontainer">
-            <p className="sobreTitle">{serviceToShow["sobreTitle"]}</p>
-            { serviceToShow.id !== 3? (<p className="sobreP">{serviceToShow["sobreParagraph"]}</p>) : (<> 
-              <p className="sobreP-3d">Ofrecemos servicios de impresión 3D para concretar tus ideas. El prototipado rápido permite realizar pruebas de concepto o mejorar tu diseño antes de pasar a la producción en serie.</p>
-              <p className="sobreP-3d">La impresión con materiales técnicos (ABS, Nylon, PETG, Flex y Hips) permite la fabricación de productos definitivos de solicitación térmica y mecánica relativamente baja, o que se adapten a necesidades particulares.</p>
-              <p className="sobreP-3d">La impresión en resina biocompatible permite la interacción de productos impresos con el cuerpo humano.</p>
-              <p className="sobreP-3d">La tecnología de escaneo 3D permite la reproducción digital de un objeto físico para poder intervenir y, eventualmente, reimprimirlo.</p>
-            </>)
-            }
-          </div>
+            <div className="pl-20">
+              <img  className="sobreimg" src={`../assets/${serviceToShow["sobreImg"]}`} alt="" />
+            </div>
+            <div className="sobreinfocontainer">
+              <p className="sobreTitle">{serviceToShow["sobreTitle"]}</p>
+              { serviceToShow.id !== 3? (<p className="sobreP">{serviceToShow["sobreParagraph"]}</p>) : (<> 
+                <p className="sobreP-3d">Ofrecemos servicios de impresión 3D para concretar tus ideas. El prototipado rápido permite realizar pruebas de concepto o mejorar tu diseño antes de pasar a la producción en serie.</p>
+                <p className="sobreP-3d">La impresión con materiales técnicos (ABS, Nylon, PETG, Flex y Hips) permite la fabricación de productos definitivos de solicitación térmica y mecánica relativamente baja, o que se adapten a necesidades particulares.</p>
+                <p className="sobreP-3d">La impresión en resina biocompatible permite la interacción de productos impresos con el cuerpo humano.</p>
+                <p className="sobreP-3d">La tecnología de escaneo 3D permite la reproducción digital de un objeto físico para poder intervenir y, eventualmente, reimprimirlo.</p>
+              </>)
+              }
+            </div>
           </div>         
         </div>
         <CasosExitoAccordion serviceToShow={serviceToShow}/>
