2192a924885bc3ac7c2e469511711776a967f642
Sebastian Tanquia <zevvaztv@gmail.com>
2023-11-15 12:59:43 -0300

    I18next implementation and translation

---
+++ src/translation/i18n.js
@@ -0,0 +1,26 @@
+// i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import tr from '../translation/translations.json';
+
+i18n
+  .use(initReactI18next) // inicializar react-i18next
+  .init({
+    resources: {
+      // Aquí coloca tus traducciones
+      en: {
+        translation: tr.nosotrosEnglish,
+      },
+      es: {
+        translation: tr.nosotros,
+      },
+      // Agrega más idiomas según sea necesario
+    },
+    lng: 'en', // idioma predeterminado
+    fallbackLng: 'en', // idioma de respaldo
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
