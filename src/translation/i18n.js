8f3310f07f568da48740d823c0fb6a6eef11ac0e
Alf <alfredoaltube@gmail.com>
2023-11-22 10:21:59 -0300

    addedtranslations

--- src/translation/i18n.js
+++ src/translation/i18n.js
@@ -16,7 +16,8 @@ i18n
           ...tr.CompanyEnglish,
           ...tr.BannerEnglish,
           ...tr.AreasTrabajoEnglish,
-          ...tr.CompañiasEnglish
+          ...tr.CompañiasEnglish,
+          ...tr.IoTBannerEnglish
         },
       },
       es: {
@@ -27,7 +28,8 @@ i18n
           ...tr.CompanyEspañol,
           ...tr.BannerEspañol,
           ...tr.AreasTrabajoEspañol,
-          ...tr.CompañiasEspañol
+          ...tr.CompañiasEspañol,
+          ...tr.IoTBanner
         },
       },
       // Agrega más idiomas según sea necesario
