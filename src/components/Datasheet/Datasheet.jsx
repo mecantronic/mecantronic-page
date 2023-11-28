cf9584255be4d083fa8e028948b46faa896d18bc
Alf <alfredoaltube@gmail.com>
2023-11-28 10:54:16 -0300

    Update Datasheet.jsx

--- src/components/Datasheet/Datasheet.jsx
+++ src/components/Datasheet/Datasheet.jsx
@@ -1,26 +1,37 @@
-import "./Datasheet.css"
-import React from "react"
-import { BannerTitles } from "../BannerTitles/BannerTitles"
+import "./Datasheet.css";
+import React from "react";
+import { BannerTitles } from "../BannerTitles/BannerTitles";
+import { useTranslation } from "react-i18next";
 
-export const Datasheet = ({product}) =>{
-    const nombreFichaTecnica = `datasheet-3d-printer-${product["title"].toLowerCase()}.pdf`
-    return(
-        <>
-        <div className='datasheet'>
-            <BannerTitles subtitle={"Ficha técnica"} title={"Conocé las especificaciones técnicas de esta impresora"}/>
-            <div>
-                {product["datasheet"].map((specification)=>{
-                    return (
-                        <div className="specification">
-                            <p>{specification["specification"]}</p>
-                        </div>
-                    )
-                })}
-            </div>
-            <a href={`../assets/products/${product["title"]}/${nombreFichaTecnica}` } target="_blank" rel="noreferrer">
-                <button>Descargar</button>
-            </a>
+export const Datasheet = ({ product }) => {
+  const { t } = useTranslation();
+  const nombreFichaTecnica = `datasheet-3d-printer-${product[
+    "title"
+  ].toLowerCase()}.pdf`;
+  return (
+    <>
+      <div className="datasheet">
+        <BannerTitles
+          subtitle={"Ficha técnica"}
+          title={"Conocé las especificaciones técnicas de esta impresora"}
+        />
+        <div>
+          {product["datasheet"].map((specification) => {
+            return (
+              <div className="specification">
+                <p>{specification["specification"]}</p>
+              </div>
+            );
+          })}
         </div>
-        </>
-    )
-}
+        <a
+          href={`../assets/products/${product["title"]}/${nombreFichaTecnica}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button>{t("sk_download")}</button>
+        </a>
+      </div>
+    </>
+  );
+};
